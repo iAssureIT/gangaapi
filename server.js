7bf38aca951383aac8e57a0d60aa614fee9b5bf8
Miss. Kharare <khararemanisha555@gmail.com>
2019-09-07 11:04:54 +0530

    Manisha added changes

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const app = require('./app'); // app file include
-const globalVariable = require('./nodemon');
-const port = process.env.PORT || globalVariable.PORT;
-const server = http.createServer(app);
-server.listen(port);
-
