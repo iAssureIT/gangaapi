acba3ea14967c893090e4a1816dd50309c3fd7f3
angelasmi <asmi3688@gmail.com>
2019-09-09 10:44:31 +0530

    Asmita pulled code

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const app = require('./app'); // app file include
+
 const globalVariable = require('./nodemon');
 const port = process.env.PORT || globalVariable.PORT;
+// const port = process.env.PORT || 3060;
+
 const server = http.createServer(app);
 server.listen(port);
