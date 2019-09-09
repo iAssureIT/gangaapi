6fa223e2babbe801d5e6e64475080a0421064858
angelasmi <asmi3688@gmail.com>
2019-09-09 14:37:44 +0530

    Asmita added console for port in serve file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = require('./app'); // app file include
 
 const globalVariable = require('./nodemon');
 const port = process.env.PORT || globalVariable.PORT;
+
+console.log("port in server js file---->",port);
 // const port = process.env.PORT || 3060;
 
 const server = http.createServer(app);
