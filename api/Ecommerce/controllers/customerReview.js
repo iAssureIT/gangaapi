010dbf388367dceea0c4931fd02a59a3bddf697a
Amit Shinde <amitrshinde156@gmail.com>
2019-09-26 18:34:50 +0530

    Amit added changes

--- api/Ecommerce/controllers/customerReview.js
+++ api/Ecommerce/controllers/customerReview.js
@@ -3,9 +3,7 @@ const mongoose  = require("mongoose");
 const CustomerReview = require('../models/customerReview');
 
 exports.insertCustomerReview = (req,res,next)=>{
-	CustomerReview.find()
-		.exec()
-		.then(data =>{
+	
 			const customerReview = new CustomerReview({
                 _id                       : new mongoose.Types.ObjectId(),                    
                 customerID                : req.body.customerID,
@@ -26,13 +24,20 @@ exports.insertCustomerReview = (req,res,next)=>{
                 res.status(500).json({
                     error: err
                 });
-            });
-		
-	})
-	.catch(err =>{
-		console.log(err);
-		res.status(500).json({
-			error: err
-		});
-	});
+            });	
+	
+};
+
+exports.listCustomerReview = (req,res,next)=>{
+    CustomerReview.find({productID : req.params.productID})
+    .exec()
+    .then(data=>{
+        res.status(200).json(data);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 }
--- api/Ecommerce/routes/customerReview.js
+++ api/Ecommerce/routes/customerReview.js
@@ -5,5 +5,7 @@ const router 	= express.Router();
 const customerReviewController = require('../controllers/customerReview');
 
 router.post('/post', customerReviewController.insertCustomerReview);
+router.get('/get/list/:productID',customerReviewController.listCustomerReview);
+
 
 module.exports = router;
