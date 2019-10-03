8f21865ac6852a7c1a8f3d0aec87baa31c570e13
Miss. Kharare <khararemanisha555@gmail.com>
2019-10-03 18:33:53 +0530

    Manisha added changes

--- api/Ecommerce/routes/products.js
+++ api/Ecommerce/routes/products.js
@@ -15,7 +15,7 @@ router.get('/get/list',productController.list_product);
 
 //router.get('/get/brandlist',productController.list_brand);
 
-router.get('/get/listbytype/:section/:productType',productController.list_productby_type);
+router.get('/get/listbytype/:productType',productController.list_productby_type);
 
 router.get('/get/listbytypeNcategory/:categoryID/:productType',productController.list_productby_type_category);
 
