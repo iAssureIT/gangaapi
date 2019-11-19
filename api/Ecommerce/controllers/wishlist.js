10bc82d55053b39fd67f28029962cb10dbb84f16
Miss. Kharare <khararemanisha555@gmail.com>
2019-11-19 20:25:45 +0530

    Manisha added changes

--- api/Ecommerce/controllers/wishlist.js
+++ api/Ecommerce/controllers/wishlist.js
@@ -154,7 +154,7 @@ exports.fetch_wishlist = (req,res,next)=>{
     });
 };
 exports.fetch_wishlist_product = (req,res,next)=>{
-    Wishlists.findOne({"customerID" : ObjectID(req.params.userID), "productID": ObjectID(req.params.productID)})
+    Wishlists.findOne({"customerID" : req.params.userID, "productID": req.params.productID})
     .exec()
     .then(data=>{
         res.status(200).json(data);
